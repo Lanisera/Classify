c744ae1514f2a4c33af069f44832a1c1b71d848d
laniser <laniser0905@gmail.com>
2023-12-04 20:15:27 +0800

    read and write matrix function

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -27,16 +27,89 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -28
+	sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)   
+    sw s4, 16(sp)   
+    sw ra, 20(sp)
+    sw s5, 24(sp)
+    
+    add s0, x0, a0
+    add s1, x0, a1
+    add s2, x0, a2
 
+    # fopen
+    add a1, x0, s0
+    add a2, x0, x0
+    jal fopen
+    addi t0, x0, -1
+    beq a0, t0, exit_90
+    add s3, x0, a0
+    
+    # get row and col
+    # row
+    add a1, x0, s3
+    add a2, x0, s1
+    addi a3, x0, 4
+    jal fread
+    addi t0, x0, 4
+    bne a0, t0, exit_91
+    #col
+    add a1, x0, s3
+    add a2, x0, s2
+    addi a3, x0, 4
+    jal fread
+    addi t0, x0, 4
+    bne a0, t0, exit_91
+    
+    lw s1, 0(s1)    # get num of row
+    lw s2, 0(s2)    # get num of col
 
-
-
-
-
-
-
+    # malloc the matrix
+    # malloc
+    mul s5, s1, s2
+    addi t0, x0, 4
+    mul s5, s5, t0
+    add a0, x0, s5
+    jal malloc
+    beq a0, x0, exit_88
+    add s4, x0, a0
+    # get the value 
+    add a1, x0, s3
+    add a2, x0, s4
+    add a3, x0, s5
+    ebreak
+    jal fread
+    bne a0, s5, exit_91
+    
+    # fclose
+    add a1, x0, s3
+    jal fclose
+    bne a0, x0, exit_92
+    
+    add a0, x0, s4
     # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw ra, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
+    
     ret
+exit_88:
+    addi a1, x0, 88
+    j exit2
+exit_90:
+    addi a1, x0, 90
+    j exit2
+exit_91:
+    addi a1, x0, 91
+    j exit2
+exit_92:
+    addi a1, x0, 92
+    j exit2
--- src/write_matrix.s
+++ src/write_matrix.s
@@ -25,16 +25,71 @@
 write_matrix:
 
     # Prologue
-
-
-
-
-
-
-
-
+    addi sp, sp, -28
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    add s0, x0, a0
+    add s1, x0, a1
+    add s2, x0, a2
+    add s3, x0, a3
+    
+    # fopen
+    add a1, x0, s0
+    addi a2, x0, 1
+    jal fopen
+    addi t0, x0, -1
+    beq t0, a0, exit_93
+    add s4, x0, a0
+    
+    # write row and col
+    addi sp, sp, -8
+    sw s2, 0(sp)
+    sw s3, 4(sp)
+    add a1, x0, s4
+    add a2, x0, sp
+    addi a3, x0, 2
+    addi a4, x0, 4
+    jal fwrite
+    addi t0, x0, 2
+    bne t0, a0, exit_94
+    addi sp, sp, 8
+    
+   # write the matrix
+    mul s5, s2, s3
+    add a1, x0, s4
+    add a2, x0, s1
+    add a3, x0, s5
+    addi a4, x0, 4
+    jal fwrite
+    bne a0, s5, exit_94
+    
+    # fclose
+    add a1, x0, s4
+    jal fclose
+    bne x0, a0, exit_95
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
 
     ret
+exit_93:
+    addi a1, x0, 93
+    j exit2
+exit_94:
+    addi a1, x0, 94
+    j exit2
+exit_95:
+    addi a1, x0, 95
+    j exit2
